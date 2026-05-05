782a61944c672651e345f1a190bca76abe529571
mcherezov <m.cherezov@redmadrobot.com>
2026-05-05 22:42:19 +0700

    fix: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/portfolio/', // <--- Добавь эту строчку (слэши по краям обязательны!)
   plugins: [
     react(),
     tailwindcss(),
